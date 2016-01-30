497027bda6c53772b825351f194719f4a0746bbc
agilecomm <jeremy.kates@charter.net>
2016-01-30 13:00:00 -0600

    Added Queue

--- Algorithms(Swift).playground/Pages/Queue.xcplaygroundpage/Contents.swift
+++ Algorithms(Swift).playground/Pages/Queue.xcplaygroundpage/Contents.swift
@@ -1,5 +1,39 @@
 //: Queue
-
-public struct Queue {
+// Generic Queue Example
+public struct Queue<T> {
     
+    private var array = [T]()
+    
+    public var count: Int {
+        return array.count
+    }
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    public mutating func enqueue(element: T) {
+        array.insert(element, atIndex: 0)
+    }
+    public mutating func dequeue() -> T? {
+        guard !isEmpty else {
+        return nil
+        }
+        return array.removeFirst()
+    }
+    public func peek() -> T? {
+        guard !isEmpty else {
+            return nil
+        }
+        return array.first
+    }
 }
+
+var queue = Queue(array: [1,2,3,4,5])
+
+queue.count
+queue.peek()
+queue.dequeue()
+queue.peek()
+queue.enqueue(1)
+print(queue)
