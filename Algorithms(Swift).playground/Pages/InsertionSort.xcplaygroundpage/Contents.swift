fe144db66e305d19fe1e799081a33d5b7a0d1515
agilecomm <jeremy.kates@charter.net>
2016-03-10 22:17:43 -0600

    Insertion Sort

---
+++ Algorithms(Swift).playground/Pages/InsertionSort.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+
+import Foundation
+
+
+func insertionSort(numberList: [Int]) -> [Int] {
+    
+    var output = numberList
+    
+    //Use enumerate on array member to get element key
+    for (primaryIndex,value) in output.enumerate() {
+        
+        let currentValue = value
+        
+        for var secondaryIndex = primaryIndex; secondaryIndex > -1; secondaryIndex-- {
+            if currentValue < output[secondaryIndex] {
+                output.removeAtIndex(secondaryIndex + 1)
+                output.insert(currentValue, atIndex: secondaryIndex)
+            }
+        }
+    }
+    return output
+}
+
+let unsortedArray = [2,28,1,2398,23,182,32,221]
+
+insertionSort(unsortedArray)
